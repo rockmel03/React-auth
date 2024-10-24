eb300b75a90a0cbc42b82399bf405d620da39e9a
rockmel03 <kamalswag2@gmail.com>
2024-10-24 21:52:51 +0530

    B- move dotenv configuration to  app.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,5 @@
-import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/connectDB.js";
-
-dotenv.config({
-  path: "./.env",
-});
 
 const port = process.env.PORT || 8080;
 connectDB()
