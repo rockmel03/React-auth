4eb9ff5553d693278ffaca4e9fa3fceeeda4ca26
rockmel03 <kamalswag2@gmail.com>
2024-10-27 19:05:29 +0530

    F- add some UI changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Register } from "./pages";
 
 export default function App() {
   return (
-    <main className="bg-blue-400">
+    <main className="bg-blue-400 w-full min-h-screen flex flex-col items-center justify-center">
       <Register />
     </main>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -91,188 +91,162 @@ export const Register = () => {
     usernameRef.current.focus();
   }, []);
 
-  return (
-    <section className="w-full min-h-screen flex flex-col items-center justify-center">
-      <div className="bg-blue-500 text-white p-5 rounded-lg  md:min-w-[320px]">
-        {success ? (
-          <div>
-            <h1 className="text-2xl font-semibold">Registration Succesful!</h1>
-            <a href="/home" className="hover:underline">
-              Back to Home
-            </a>
-          </div>
-        ) : (
-          <>
-            {" "}
-            <h1 className="text-2xl font-semibold">Register</h1>
-            <br />
-            <p
-              ref={errRef}
-              aria-live="assertive"
-              className={`px-2 py-1 rounded w-full bg-red-200 text-red-800 ${
-                !error && "hidden"
-              } transition-all duration-300 ease`}
-            >
-              {error}
-            </p>
-            <form
-              onSubmit={handleSubmit}
-              className="w-full flex flex-col gap-1"
-            >
-              <label htmlFor="username">
-                Username{" "}
-                {username && validUsername && (
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    style={{ color: "#00ff4c" }}
-                  />
-                )}
-                {username && !validUsername && (
-                  <FontAwesomeIcon
-                    icon={faCircleXmark}
-                    style={{ color: "#ff0000" }}
-                  />
-                )}
-              </label>
-              <input
-                className={`px-2 py-1 rounded bg-white text-black w-full ${
-                  focusUser && username && !validUsername
-                    ? "outline-red-500"
-                    : ""
-                }`}
-                type="text"
-                id="username"
-                autoComplete="false"
-                ref={usernameRef}
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                onFocus={() => setFocusUser(true)}
-                onBlur={() => setFocusUser(false)}
-                aria-invalid={validUsername ? "false" : "true"}
-                aria-describedby="usrNote"
-                required
-              />
-              <p
-                id="usrNote"
-                className={`${
-                  focusUser && username && !validUsername ? "block" : "hidden"
-                } text-xs bg-zinc-800 p-2 rounded`}
-              >
-                4 to 24 characters.
-                <br />
-                Must begin with a letter.
-                <br />
-                Letters, numbers, underscores, hyphens allowed.
-              </p>
-              <label htmlFor="password">
-                Password{" "}
-                {password && validPwd && (
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    style={{ color: "#00ff4c" }}
-                  />
-                )}
-                {password && !validPwd && (
-                  <FontAwesomeIcon
-                    icon={faCircleXmark}
-                    style={{ color: "#ff0000" }}
-                  />
-                )}
-              </label>
-              <input
-                className={`px-2 py-1 rounded bg-white text-black w-full ${
-                  focusPwd && password && !validPwd ? "outline-red-500" : ""
-                }`}
-                type="password"
-                id="password"
-                ref={passwordRef}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onFocus={() => setFocusPwd(true)}
-                onBlur={() => setFocusPwd(false)}
-                aria-invalid={validPwd ? "false" : " true"}
-                aria-describedby="pwdNote"
-                required
-              />
-              <p
-                id="pwdNote"
-                className={`${
-                  focusPwd && password && !validPwd ? "block" : "hidden"
-                } text-xs bg-zinc-800 p-2 rounded`}
-              >
-                8 to 24 characters.
-                <br />
-                Must include uppercase and lowercase letters, a number and a
-                special character.
-                <br />
-                Allowed special characters:{" "}
-                <span aria-label="exclamation mark">!</span>{" "}
-                <span aria-label="at symbol">@</span>{" "}
-                <span aria-label="hashtag">#</span>{" "}
-                <span aria-label="dollar sign">$</span>{" "}
-                <span aria-label="percent">%</span>
-              </p>
-              <label htmlFor="cnf-password">
-                Confirm Password{" "}
-                {cnfPassword && isPwdMatch && (
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    style={{ color: "#00ff4c" }}
-                  />
-                )}
-                {cnfPassword && !isPwdMatch && (
-                  <FontAwesomeIcon
-                    icon={faCircleXmark}
-                    style={{ color: "#ff0000" }}
-                  />
-                )}
-              </label>
-              <input
-                className={`px-2 py-1 rounded bg-white text-black w-full ${
-                  focusCnfPwd && cnfPassword && !isPwdMatch
-                    ? "outline-red-500"
-                    : ""
-                }`}
-                type="password"
-                id="cnf-password"
-                value={cnfPassword}
-                onChange={(e) => setCnfPassword(e.target.value)}
-                onFocus={() => setFocusCnfpwd(true)}
-                onBlur={() => setFocusCnfpwd(false)}
-                aria-invalid={isPwdMatch ? "false" : "true"}
-                aria-describedby="cnfNote"
-                required
-              />
-              <p
-                id="cnfNote"
-                className={`${
-                  focusCnfPwd && cnfPassword && !isPwdMatch ? "block" : "hidden"
-                } text-xs bg-zinc-800 p-2 rounded`}
-              >
-                Must match the first password input field
-              </p>
+  return success ? (
+    <div className="min-h-[400px] bg-blue-500 text-white p-5 rounded-lg md:min-w-[320px]">
+      <h1 className="text-2xl font-semibold">Registration Succesful!</h1>
+      <a href="/home" className="hover:underline">
+        Back to Home
+      </a>
+    </div>
+  ) : (
+    <section className="bg-blue-500 text-white p-5 rounded-lg  md:min-w-[320px]">
+      <p
+        ref={errRef}
+        aria-live="assertive"
+        className={`px-2 py-1 rounded w-full bg-red-200 text-red-800 ${
+          !error && "hidden"
+        } transition-all duration-300 ease`}
+      >
+        {error}
+      </p>
+      <h1 className="text-2xl font-semibold my-2">Register</h1>
 
-              <button
-                className="px-2 py-2 mt-2 rounded bg-blue-700 hover:bg-blue-800 cursor-pointer disabled:opacity-75 disabled:cursor-not-allowed"
-                type="submit"
-                disabled={
-                  !validUsername || !validPwd || !isPwdMatch ? true : false
-                }
-              >
-                Register
-              </button>
-            </form>
-            <br />
-            <p className="text-sm">
-              Already have an account?{" "}
-              {/* TODO: user react router Link component */}
-              <a href="/login" className="hover:underline">
-                Login here.
-              </a>
-            </p>
-          </>
-        )}
-      </div>
+      <form onSubmit={handleSubmit} className="w-full flex flex-col gap-1">
+        <label htmlFor="username">
+          Username{" "}
+          {username && validUsername && (
+            <FontAwesomeIcon icon={faCheck} style={{ color: "#00ff4c" }} />
+          )}
+          {username && !validUsername && (
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              style={{ color: "#ff0000" }}
+            />
+          )}
+        </label>
+        <input
+          className={`px-2 py-1 rounded bg-white text-black w-full ${
+            focusUser && username && !validUsername ? "outline-red-500" : ""
+          }`}
+          type="text"
+          id="username"
+          autoComplete="off"
+          ref={usernameRef}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          onFocus={() => setFocusUser(true)}
+          onBlur={() => setFocusUser(false)}
+          aria-invalid={validUsername ? "false" : "true"}
+          aria-describedby="usrNote"
+          required
+        />
+        <p
+          id="usrNote"
+          className={`${
+            focusUser && username && !validUsername ? "block" : "hidden"
+          } text-xs bg-zinc-800 p-2 rounded`}
+        >
+          4 to 24 characters.
+          <br />
+          Must begin with a letter.
+          <br />
+          Letters, numbers, underscores, hyphens allowed.
+        </p>
+        <label htmlFor="password">
+          Password{" "}
+          {password && validPwd && (
+            <FontAwesomeIcon icon={faCheck} style={{ color: "#00ff4c" }} />
+          )}
+          {password && !validPwd && (
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              style={{ color: "#ff0000" }}
+            />
+          )}
+        </label>
+        <input
+          className={`px-2 py-1 rounded bg-white text-black w-full ${
+            focusPwd && password && !validPwd ? "outline-red-500" : ""
+          }`}
+          type="password"
+          id="password"
+          ref={passwordRef}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onFocus={() => setFocusPwd(true)}
+          onBlur={() => setFocusPwd(false)}
+          aria-invalid={validPwd ? "false" : " true"}
+          aria-describedby="pwdNote"
+          required
+        />
+        <p
+          id="pwdNote"
+          className={`${
+            focusPwd && password && !validPwd ? "block" : "hidden"
+          } text-xs bg-zinc-800 p-2 rounded`}
+        >
+          8 to 24 characters.
+          <br />
+          Must include uppercase and lowercase letters, a number and a special
+          character.
+          <br />
+          Allowed special characters:{" "}
+          <span aria-label="exclamation mark">!</span>{" "}
+          <span aria-label="at symbol">@</span>{" "}
+          <span aria-label="hashtag">#</span>{" "}
+          <span aria-label="dollar sign">$</span>{" "}
+          <span aria-label="percent">%</span>
+        </p>
+        <label htmlFor="cnf-password">
+          Confirm Password{" "}
+          {cnfPassword && isPwdMatch && (
+            <FontAwesomeIcon icon={faCheck} style={{ color: "#00ff4c" }} />
+          )}
+          {cnfPassword && !isPwdMatch && (
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              style={{ color: "#ff0000" }}
+            />
+          )}
+        </label>
+        <input
+          className={`px-2 py-1 rounded bg-white text-black w-full ${
+            focusCnfPwd && cnfPassword && !isPwdMatch ? "outline-red-500" : ""
+          }`}
+          type="password"
+          id="cnf-password"
+          value={cnfPassword}
+          onChange={(e) => setCnfPassword(e.target.value)}
+          onFocus={() => setFocusCnfpwd(true)}
+          onBlur={() => setFocusCnfpwd(false)}
+          aria-invalid={isPwdMatch ? "false" : "true"}
+          aria-describedby="cnfNote"
+          required
+        />
+        <p
+          id="cnfNote"
+          className={`${
+            focusCnfPwd && cnfPassword && !isPwdMatch ? "block" : "hidden"
+          } text-xs bg-zinc-800 p-2 rounded`}
+        >
+          Must match the first password input field
+        </p>
+
+        <button
+          className="px-2 py-2 mt-2 rounded bg-blue-700 hover:bg-blue-800 cursor-pointer disabled:opacity-75 disabled:cursor-not-allowed"
+          type="submit"
+          disabled={!validUsername || !validPwd || !isPwdMatch ? true : false}
+        >
+          Register
+        </button>
+      </form>
+      <br />
+      <p className="text-sm">
+        Already have an account? {/* TODO: user react router Link component */}
+        <a href="/login" className="hover:underline">
+          Login here.
+        </a>
+      </p>
     </section>
   );
 };
