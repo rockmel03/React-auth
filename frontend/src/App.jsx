c94863e15e25fd7f8323636a01b938cbe088860a
rockmel03 <kamalswag2@gmail.com>
2024-10-28 22:58:54 +0530

    F- add RequireAuth component and make routes protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Layout } from "./components";
+import { Layout, RequireAuth } from "./components";
 import { Register, Login, Home, Admin, Editor, ErrorPage } from "./pages";
 
 export default function App() {
@@ -11,9 +11,11 @@ export default function App() {
         <Route path="/login" element={<Login />} />
 
         {/* protected routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/editor" element={<Editor />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/editor" element={<Editor />} />
+        </Route>
 
         {/* default route */}
         <Route path="*" element={<ErrorPage />} />
