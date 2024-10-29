ec08894d7b410bd99402adc732a4d68693c84ab8
rockmel03 <kamalswag2@gmail.com>
2024-10-29 20:13:13 +0530

    F- add elements to the page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <section className="bg-blue-500 text-white p-5 rounded-lg md:min-w-[320px]">
+      <h1 className="text-2xl font-semibold">Error 404 - Page Not Found</h1>
+      <p>The page you are trying to access does not exist.</p>
+      <Link to="/" className="hover:underline">
+        Back to Home
+      </Link>
+    </section>
+  );
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="bg-blue-500 text-white p-5 rounded-lg md:min-w-[320px]">
+      <h1 className="text-xl font-medium">You are logged in!😀</h1>
+      <br />
+      <Link to={`/profile/`} className="hover:underline">
+        😎 See your Profile
+      </Link>
+      <br />
+      <Link to="/editor" className="hover:underline">
+        ✍️ Go to Editor Page
+      </Link>
+      <br />
+      <Link to="/admin" className="hover:underline">
+        🥷 Go to Admin Page
+      </Link>
+    </section>
+  );
+};
