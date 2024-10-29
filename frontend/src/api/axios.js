422d74a5f4a87c338c2452a94613c20d7b360fa2
rockmel03 <kamalswag2@gmail.com>
2024-10-29 23:53:51 +0530

    F - create apiPrivate instance

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080";
+
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
+});
+
+export const apiPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true, // Enable cookies for cross-origin requests
 });
 
-export default api
+export default api;
