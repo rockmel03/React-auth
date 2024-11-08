3831f672f78c576b91896e2d9cf132e6bd64e1a7
rockmel03 <kamalswag2@gmail.com>
2024-11-08 23:10:27 +0530

    F- add more functionality to useLocal strorage

--- frontend/src/hooks/useLocalStorage.js
+++ frontend/src/hooks/useLocalStorage.js
@@ -1,9 +1,23 @@
 import { useState, useEffect } from "react";
 
+const getLocalValue = (key, initialValue) => {
+  // SSR => next.js
+  if (typeof window === "undefined") return initialValue;
+
+  // if a value already stored
+  const localValue = JSON.parse(localStorage.getItem(key));
+  if (localValue) return localValue;
+
+  // return result of a function
+  if (initialValue instanceof Function) return initialValue();
+
+  return initialValue;
+};
+
 const useLocalStorage = (key, initialValue) => {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initialValue
-  );
+  const [value, setValue] = useState(() => {
+    return getLocalValue(key, initialValue);
+  });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
