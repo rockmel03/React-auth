85dd2a4c4d7915f8ceac6c95b699c2f6f6a19d78
rockmel03 <kamalswag2@gmail.com>
2024-11-11 15:17:05 +0530

    F - disable react dev tools in production mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+// Disable React Developer Tools in production mode
+/**if(process.env.NODE_ENV === 'production')*/
+if (import.meta.env.PROD) {
+  disableReactDevTools();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
