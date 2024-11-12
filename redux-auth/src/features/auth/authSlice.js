d1455116a93fd7c0646d72424d17a6e1c7b1a7bc
rockmel03 <kamalswag2@gmail.com>
2024-11-12 15:08:18 +0530

    RA - create authSlice : create auth state and reducers then export them

---
+++ redux-auth/src/features/auth/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { user: null, token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { user, accessToken } = action.payload;
+      state.user = user;
+      state.token = accessToken;
+    },
+    logOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+
+export default authSlice.reducer;
+
+// selectors functions
+export const selectCurrentUser = (state) => state.user;
+export const selectCurrentToken = (state) => state.token;
