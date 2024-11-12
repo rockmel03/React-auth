6ec11944fe1340c2dd0c85cedeab51d92899ce18
rockmel03 <kamalswag2@gmail.com>
2024-11-12 17:57:36 +0530

    RA - feat: enable React Router v7 future flags for smoother transitions and updated route matching

--- redux-auth/src/main.jsx
+++ redux-auth/src/main.jsx
@@ -10,7 +10,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true, //Enabled `v7_relativeSplatPath` to prepare for updated splat route matching in v7
+          v7_startTransition: true, // Enabled `v7_startTransition` to improve route transition performance
+        }}
+      >
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
